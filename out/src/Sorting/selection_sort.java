1cd1d26969372ce2ec40542ee9d0be3b05477937
RANJEET KUMAR <kumarranjeet13950@gmail.com>
2025-04-14 01:00:27 +0530

    Another approach of Selection-Sort

--- out/src/Sorting/selection_sort.java
+++ out/src/Sorting/selection_sort.java
@@ -3,7 +3,7 @@ package Sorting;
 public class selection_sort {
     public static void main(String[] args) {
         int arr[] = {1,5,4,8,6,9};
-        selection(arr);
+        secondMethod(arr);
        for (int i = 0; i<arr.length; i++){
            System.out.print(arr[i]+ " ");
        }
@@ -22,6 +22,21 @@ public class selection_sort {
             int temp = arr[minpos];
             arr[minpos] = arr[i];
             arr[i] = temp;
+        }
+    }
+
+    //Second approach//
+    static void secondMethod(int arr[]){
+        for (int i = 0; i<arr.length-1; i++){
+            for (int j = i+1; j>0; j--){
+                if (arr[j]<arr[j-1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
+                }else{
+                    break;
+                }
+            }
         }
     }
 }
